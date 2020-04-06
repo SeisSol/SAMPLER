6bc5861689aab3509a9660c29a9269c60396c87a
Maximilian Schmeller <m.schmeller@tum.de>
2020-04-06 12:52:37 +0200

    Scrapped last week's approach in favor for the quadtree approach [Work in progress]

--- src/quadtrees.jl
+++ src/quadtrees.jl
@@ -5,48 +5,53 @@
 =#
 
 module Quadtrees
-    export Point, Quadtree, quadtree_insert!, Quad
-
-    struct Point{T}
-        x       :: Float64
-        y       :: Float64
-        content :: T
-    end
+    export Quadtree, quadtree_insert!, Quad, get_tetrahedra_at
 
     struct Quad
-        x       :: Float64
-        y       :: Float64
-        width   :: Float64
-        height  :: Float64
+        x        :: Float64
+        y        :: Float64
+        width    :: Float64
+        height   :: Float64
     end
 
-    mutable struct Quadtree{T}
-        region  :: Quad
-        xn_yn   :: Union{Quadtree{T}, Point{T}, Nothing}
-        xp_yn   :: Union{Quadtree{T}, Point{T}, Nothing}
-        xn_yp   :: Union{Quadtree{T}, Point{T}, Nothing}
-        xp_yp   :: Union{Quadtree{T}, Point{T}, Nothing}
+    mutable struct Quadtree
+        region   :: Quad
+        contents :: AbstractArray{Tuple{Tuple{Float64,Float64,Float64,Float64}, Int32},1}
+        xn_yn    :: Union{Quadtree, Nothing}
+        xp_yn    :: Union{Quadtree, Nothing}
+        xn_yp    :: Union{Quadtree, Nothing}
+        xp_yp    :: Union{Quadtree, Nothing}
     end
 
-    @enum Quadrant xn_yn xp_yn xn_yp xp_yp
+    @enum Quadrant center xn_yn xp_yn xn_yp xp_yp
 
-    function Quadtree(region::Quad, points::AbstractArray{Point{T}}) where T
-        qt = Quadtree{T}(deepcopy(region), nothing, nothing, nothing, nothing)
+    function Quadtree(region::Quad, tetrahedra::AbstractArray{Int64, 2}, points::AbstractArray{Float64, 2})
+        qt = Quadtree(deepcopy(region), Array{Int32,1}(), nothing, nothing, nothing, nothing)
 
-        for point in points
-            quadtree_insert!(qt, point)
+        num_tetrahedra = size(tetrahedra, 2)
+        for tet_id :: Int32 ∈ 1:num_tetrahedra
+            if tet_id % 10000 == 0; println("Progress: tet $tet_id of $num_tetrahedra, $(tet_id/num_tetrahedra*100)% done") end
+
+            tetrahedron = tetrahedra[:, tet_id]
+            tet_points = [points[dim, point_id] for point_id in tetrahedron, dim in 1:3] # shape(t_points) = (4×3)
+            tet_aabb = (minimum(tet_points[:, 1]), minimum(tet_points[:, 2]), 
+                        maximum(tet_points[:, 1]), maximum(tet_points[:, 2]))
+            quadtree_insert!(qt, tet_aabb, tet_id)
         end
+
+        return qt
     end
 
-    function quadtree_insert!(qt::Quadtree{T}, point::Point{T}) where T
-        q = get_quadrant(qt.region, point.x, point.y)
+    function quadtree_insert!(qt::Quadtree, bounds::Tuple{Float64,Float64, Float64, Float64}, tet_id::Int32)
+        q = get_quadrant(qt.region, bounds)
+        if q == center 
+            push!(qt.contents, (bounds, tet_id))
+            return
+        end
+
         current_elem = qt[q]
 
         if isnothing(current_elem)
-            qt[q] = point
-        elseif current_elem isa Quadtree
-            quadtree_insert!(current_elem, point)
-        else # current_elem isa Point
             new_width = qt.region.width / 2
             new_height = qt.region.height / 2
 
@@ -57,18 +62,61 @@ module Quadtrees
 
             new_region = Quad(new_x, new_y, new_width, new_height)
 
-            new_tree = Quadtree{T}(new_region, nothing, nothing, nothing, nothing)
+            new_tree = Quadtree(new_region, Array{Int32,1}(), nothing, nothing, nothing, nothing)
             qt[q] = new_tree
-            quadtree_insert!(new_tree, current_elem)
-            quadtree_insert!(new_tree, point)
+            quadtree_insert!(new_tree, bounds, tet_id)
+            quadtree_insert!(new_tree, bounds, tet_id)
+        else # current_elem isa Quadtree
+            quadtree_insert!(current_elem, bounds, tet_id)
         end
     end
 
-    function get_quadrant(region::Quad, x::Real, y::Real)
+    function get_tetrahedra_at(qt::Quadtree,x::Float64, y::Float64) :: Array{NTuple{4,Float64},1}
+        tetrahedra = []
+
+        current_node = qt
+        while !isnothing(current_node)
+            for (aabb, tet_id) ∈ current_node.contents
+                if x ≥ aabb[1] && x ≤ aabb[3] && y ≥ aabb[2] && y ≤ aabb[4]
+                    push!(tetrahedra, aabb)
+                end
+            end
+
+            current_node = current_node[get_quadrant(current_node.region, x, y)]
+        end
+
+        return tetrahedra
+        
+    end
+
+    function get_quadrant(region::Quad, bounds::Tuple{Float64, Float64, Float64, Float64})
         # For numerical accuracy, coordinates are subtracted before comparing position within region
         # (coordinates have a larger domain than coordinates within a region)
-        is_left = x - region.x <= region.width / 2
-        is_btm = y - region.y <= region.height / 2
+
+        # x_max is on the left of the center?
+        is_left = bounds[3] - region.x <= region.width * .5
+        # y_max is at the bottom of the center?
+        is_btm = bounds[4] - region.y <= region.height * .5
+
+        if !is_left
+            is_right = bounds[1] - region.x > region.width * .5
+            if !is_right return center end
+        end
+
+        if !is_btm
+            is_top = bounds[2] - region.y > region.height * .5
+            if !is_top return center end
+        end
+
+        if is_left  && is_btm  return xn_yn end
+        if is_left  && !is_btm return xn_yp end
+        if !is_left && is_btm  return xp_yn end
+        if !is_left && !is_btm return xp_yp end
+    end
+
+    function get_quadrant(region::Quad, x::Float64, y::Float64)
+        is_left = x - region.x <= region.width * .5
+        is_btm = y - region.y <= region.height * .5
 
         if is_left  && is_btm  return xn_yn end
         if is_left  && !is_btm return xn_yp end
@@ -81,12 +129,15 @@ module Quadtrees
         if key == xn_yp return collection.xn_yp end
         if key == xp_yn return collection.xp_yn end
         if key == xp_yp return collection.xp_yp end
+
+        throw(ArgumentError("Illegal key: $key"))
     end
 
-    function Base.setindex!(collection::Quadtree{T}, value::Union{Quadtree{T}, Point{T}, Nothing}, key::Quadrant) where T
-        if key == xn_yn collection.xn_yn = value end
-        if key == xn_yp collection.xn_yp = value end
-        if key == xp_yn collection.xp_yn = value end
-        if key == xp_yp collection.xp_yp = value end
+    function Base.setindex!(collection::Quadtree, value::Union{Quadtree, Nothing}, key::Quadrant)
+        if key == xn_yn collection.xn_yn = value
+        elseif key == xn_yp collection.xn_yp = value
+        elseif key == xp_yn collection.xp_yn = value
+        elseif key == xp_yp collection.xp_yp = value
+        else throw(ArgumentError("Illegal key: $key")) end
     end
 end
--- src/xdmf.jl
+++ src/xdmf.jl
@@ -2,9 +2,11 @@ module XDMF
     using XMLDict
     using Mmap
     using Base.Threads
-    using Traceur
+    using LinearAlgebra
+    using Test
+    include("quadtrees.jl")
 
-    export XDMFFile, PolygonBounds, rasterize_convex_hull!, mmap_data_item, calculate_convex_hull_points, calculate_overlap!
+    export XDMFFile, PolygonBounds, mmap_data_item
 
     struct SparseSamplingRaster
         raster_cells::Dict{Tuple{Int32, Int32}, Array{Int32,1}}
@@ -59,52 +61,42 @@ module XDMF
         sampling_dx = (domain_x[end] - domain_x[1]) / num_sampling_columns[1]
         sampling_dy = (domain_y[end] - domain_y[1]) / num_sampling_columns[2]
 
-        rasters = [
-            SparseSamplingRaster(Dict{Tuple{Int32, Int32}, Array{Int32, 1}}(), 
-                           domain_x[1], domain_x[end], sampling_dx,
-                           domain_y[1], domain_y[end], sampling_dy)
-            for i ∈ 1:nthreads()
-        ]
-
-        total_tetrahedra_processed = Threads.Atomic{Int}(0)
-        last_printed_processed = 0
-        
         # shape(tetrahedra) = 4×n; iterate over n
-        num_tetrahedra = size(tetrahedra, 2)
+        region = Quadtrees.Quad(domain_x[1], domain_y[1], domain_x[2] - domain_x[1], domain_y[2] - domain_y[1])
+        qt = Quadtrees.Quadtree(region, tetrahedra, points)
 
-        Threads.@threads for i ∈ 1:num_tetrahedra
-            if threadid() == 2
-                processed = total_tetrahedra_processed[]
-                if processed - last_printed_processed ≥ 10000
-                    println("Processed $processed of $num_tetrahedra tetrahedra... $(processed/num_tetrahedra*100)% done.")
-                    flush(stdout)
-                    last_printed_processed = processed
-                end
-            end
-
-            tetrahedron = tetrahedra[:, i] # shape(tetrahedron) = 4×1
-            tetrahedron = [points[dim, point_id] for point_id in tetrahedron, dim in 1:3] # shape(tetrahedron) = 4×3
-
-            calculate_overlap!(tetrahedron, rasters[threadid()], i)
-            atomic_add!(total_tetrahedra_processed, 1)
-        end
-
-        # Collect sampling data from threads
-        raster = DenseSamplingRaster([Array{Int32,1}() for i ∈ 1:num_sampling_columns[1], j ∈ 1:num_sampling_columns[2]],
-                                     domain_x[1], domain_x[end], sampling_dx,
-                                     domain_y[1], domain_y[end], sampling_dy)
-
-        for i ∈ 1:length(rasters)
-            r = rasters[i]
-            for ((y, x), ls) ∈ pairs(r.raster_cells)
-                append!(raster.raster_cells[y, x], ls)
-            end
-
-            rasters[i] = nothing
-        end
-
-        show(IOContext(stdout, :limit=>true), "text/plain", raster.raster_cells)
         # return XDMFFile()
+    end
+
+    function is_point_in_tetrahedron(p::AbstractArray{Float64, 1}, tetrahedron::AbstractArray{Float64,2}) :: Bool
+        for i ∈ 1:4
+            # Exclude point i from the tetrahedron and consider the plane defined by the other 3 points
+            # If and only if (x,y,z) lies on the "positive" side (in normal direction) of all 4 planes, 
+            # it is inside the tetrahedron
+            p_excl = tetrahedron[i,:]
+            p1 = tetrahedron[(i%4)+1,:]
+            p2 = tetrahedron[((i+1)%4)+1,:]
+            p3 = tetrahedron[((i+2)%4)+1,:]
+
+            # Calculate Hesse normal form of the plane defined by p1, p2, p3
+            # Normal vector
+            n = normalize(cross(p2-p1, p3-p1))
+
+            # n1x1 + n2x2 + n3x3 + d = 0; solve for d
+            d = -dot(p1, n)
+
+            # If p_excl is ALWAYS on the "positive" side of the plane. 
+            # So, if p_excl and p lie on the same side, p is inside the tetrahedron
+            dist_p = dot(p, n) + d
+            dist_p_excl = dot(p_excl, n) + d
+
+            # If signs of distance vectors do not match, p and p_excl are on different sides of the plane (reject)
+            if ((dist_p <= 0 && dist_p_excl > 0) || (dist_p > 0 && dist_p_excl <= 0))
+                return false
+            end
+        end
+
+        return true
     end
 
     function mmap_data_item(data_item::XMLDict.XMLDictElement, base_path::AbstractString)
@@ -143,143 +135,5 @@ module XDMF
         filename = data_item[""] # No joke, that's how the XMLDict library stores the text in an XML tag 😂
         filename = joinpath(base_path, filename)
         return Mmap.mmap(filename, Array{number_type,length(dimensions)}, dimensions)
-    end
-
-    function calculate_overlap!(tetrahedron::AbstractArray{Float64,2}, raster::SparseSamplingRaster, tetrahedron_id::Int)
-        px_min = minimum(tetrahedron[:,1]); px_max = maximum(tetrahedron[:,1])
-        py_min = minimum(tetrahedron[:,2]); py_max = maximum(tetrahedron[:,2])
-
-        # A list of either 1 or 2 disjoint triangles that make up the tetrahedron's complex hull
-        # when projection it onto the XY-Plane
-        convex_hull = calculate_convex_hull_points(tetrahedron)
-        rasterize_convex_hull!(convex_hull, PolygonBounds(px_min, px_max, py_min, py_max), raster, tetrahedron_id)
-    end
-
-    function calculate_convex_hull_points(tetrahedron::AbstractArray{Float64,2}) :: AbstractArray{Float64,2}
-        # Calculate convex hull (in top-down view, 2D) with Graham's Algorithm
-        # The anchor point p0 is the point with the lowest y-coordinate.
-        # Should there be >1 lowest points, the one with the lowest x-coordinate of them is chosen.
-        # Therefore, sort by x-coord first and then take the first point with the lowest y-coordinate
-        tetrahedron = sortslices(tetrahedron, dims = 1, lt = (a, b)->a[1] < b[1])
-        index_y_min = argmin(tetrahedron[:,2])
-        p0 = tetrahedron[index_y_min,:]
-
-        function p1_clockwise_of_p2(p1::AbstractArray{Float64,1}, p2::AbstractArray{Float64,1}) :: Bool
-            # Rotate p1 90° CCW around origin: p1' := (-p1y, p1x)
-            # The scalar product p1'*p2 is then 0 if they are aligned, > 0 if p1 is CW from p2 and < 0 otherwise
-            -p1[2]*p2[1] + p1[1]*p2[2] > 0
-        end
-
-        remaining = [tetrahedron[1:index_y_min - 1,:]; tetrahedron[index_y_min + 1:end,:]] # cut row of p0 out of list
-        # sort by polar angle relative to x-axis parallel through p0
-        remaining = sortslices(remaining, dims = 1, lt = p1_clockwise_of_p2)
-
-        # if the angle curls clockwise at any point (turns out, when 3 points sorted by angle are left, 
-        # that can only be the angle below), remove that point from the convex hull
-        if p1_clockwise_of_p2(remaining[1,:] .- remaining[2,:], remaining[3,:] .- remaining[2,:])
-            remaining = [transpose(remaining[1,:]); transpose(remaining[3,:])]
-        end
-
-        # This is our convex hull in counter-clockwise order!
-        return [transpose(p0); remaining]
-    end
-
-    function rasterize_convex_hull!(convex_hull::AbstractArray{Float64,2}, bounds::PolygonBounds, raster::SparseSamplingRaster, tetrahedron_id::Int)
-        # This is the bounding box of all grid columns that could be affected by the tetrahedron
-        px_min = bounds.x_min; px_max = bounds.x_max
-        py_min = bounds.y_min; py_max = bounds.y_max
-
-        # Index of the row/column that contains the respective coordinate 'num', in 'raster'
-        idx(num::Float64, delta::Float64, offset::Float64)::Int = floor(Int, (num - offset) / delta) + 1
-
-        # The index offset (relative to 'raster') the 'overlap_grid' below starts at.
-        overlap_start_idx_x = idx(px_min, raster.dx, raster.x_min)
-        overlap_start_idx_y = idx(py_min, raster.dy, raster.y_min)
-        overlap_end_idx_x = idx(px_max, raster.dx, raster.x_min)
-        overlap_end_idx_y = idx(py_max, raster.dy, raster.y_min)
-
-        nx = overlap_end_idx_x - overlap_start_idx_x
-        ny = overlap_end_idx_y - overlap_start_idx_y
-
-        overlap_grid = zeros(Bool, (ny, nx))
-
-        # This function returns the index in the above grid obtained from float coordinates lying inside it
-        # +1 as Julia is 1-indexed
-        overlap_idx_x(x::Float64) = idx(x, raster.dx, raster.x_min) - overlap_start_idx_x + 1
-        overlap_idx_y(y::Float64) = idx(y, raster.dy, raster.y_min) - overlap_start_idx_y + 1
-
-        overlap_pos_x(idx_x::Number) = idx_x * raster.dx + raster.x_min
-        overlap_pos_y(idx_y::Number) = idx_y * raster.dy + raster.y_min
-
-        # For each cell (x,y) in the domain of overlap_grid, check overlap with polygon
-        Threads.@threads for idx_x ∈ overlap_start_idx_x:overlap_end_idx_x-1
-            left = overlap_pos_x(idx_x); right = overlap_pos_x(idx_x + 1)
-        
-            for idx_y ∈ overlap_start_idx_y:overlap_end_idx_y-1
-                bottom = overlap_pos_y(idx_y); top = overlap_pos_y(idx_y + 1)
-
-                # For each line between two individual points
-                n_points = size(convex_hull, 1)
-                for i ∈ 1:n_points
-                    (x0, y0) = Tuple(convex_hull[i,1:2]) # Only x- and y-coordinate are of importance
-                    (x1, y1) = Tuple(convex_hull[i % n_points + 1,1:2])
-
-                    # Mark cell according to Liang-Barsky clipping algorithm
-                    # Adapted from the Algorithm on page 7 of their "A New Concept and Method for Line Clipping" paper from 1984
-                    function clipt!(p::Float64, q::Float64, t0t1::T0T1)
-                        if p < 0
-                            r = q/p
-                            if r > t0t1.t1; return false
-                            elseif r > t0t1.t0; t0t1.t0 = r end
-                        elseif p > 0
-                            r = q/p
-                            if r < t0t1.t0; return false
-                            elseif r < t0t1.t1; t0t1.t1 = r end
-                        else # p = 0
-                            if q < 0; return false end
-                        end
-
-                        return true
-                    end
-
-                    function line_intersects_square()
-                        t0t1 = T0T1(0., 1.)
-                        Δx = x1 - x0
-                        if clipt!(-Δx, x0 - left, t0t1) && clipt!(Δx, right - x0, t0t1)
-                            Δy = y1 - y0
-                            return clipt!(-Δy, y0 - bottom, t0t1) && clipt!(Δy, top - y0, t0t1)
-                        else
-                            return false
-                        end
-                    end
-
-                    if line_intersects_square()
-                        overlap_grid[idx_y-overlap_start_idx_y+1, idx_x-overlap_start_idx_x+1] = true
-                    end
-                end
-            end
-        end
-
-        # Flood-fill the convex hull, the inside of which is now fully enclosed by the marked cells on its borders
-        for x ∈ 1:size(overlap_grid, 2)
-            y0 = 1; y1 = size(overlap_grid, 1)
-
-            # Find starting and ending marked cell in the current column. If found, fill all between
-            while y0 < y1 && !overlap_grid[y0, x]; y0 += 1 end
-            while y0 < y1 && !overlap_grid[y1, x]; y1 -= 1 end
-            # Start on the cell AFTER the starting marked cell (no need to re-mark it)
-            while (y0 += 1) < y1; overlap_grid[y0, x] = true end # Fill cells BETWEEN (excl. borders) starting and ending marked cell (if applicable) 
-        end
-
-        for x ∈ 1:size(overlap_grid, 2), y ∈ 1:size(overlap_grid, 1)
-            if overlap_grid[y, x]
-                key = (overlap_start_idx_y+y-1, overlap_start_idx_x+x-1)
-                if !haskey(raster.raster_cells, key)
-                    raster.raster_cells[key] = [tetrahedron_id]
-                else
-                    push!(raster.raster_cells[key], tetrahedron_id)
-                end
-            end
-        end
     end
 end
