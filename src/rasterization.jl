c9e51ddda41a43ca90e1911c8b23c64b6a2bfcfa
Maximilian Schmeller <m.schmeller@tum.de>
2020-04-19 16:01:19 +0200

    Prefetching variable data to avoid random hard drive accesses. Imporved performance by >9000 (at least!)

--- src/rasterization.jl
+++ src/rasterization.jl
@@ -101,6 +101,12 @@ module Rasterization
                 bin_ids[tet_id] = (bin_min == bin_max) ? bin_min : 0
             end
         end
+
+        bin_counts = zeros(Int32, n_threads + 1)
+        for bin_id in bin_ids
+            bin_counts[bin_id+1] += 1
+        end
+
         println("Done.")
 
         #============================================#
@@ -117,18 +123,12 @@ module Rasterization
                      "y", "x", "time")
         end
 
-        #============================================#
-        # Allocate NetCDF output arrays
-        #============================================#
+        iteration_grids = Array{Array{Float64, 3}, 1}(undef, n_vars)
+        for var_id ∈ 1:n_vars
+            iteration_grids[var_id] = Array{Float64, 3}(undef, (num_samples_y, num_samples_x, n_times_per_iteration))
+        end
 
-        print("Allocating data structures... ")
-
-        grid_running_avg_u = Array{Float64, 3}(undef, (num_samples_y, num_samples_x, n_times_per_iteration))
-        grid_running_avg_v = Array{Float64, 3}(undef, (num_samples_y, num_samples_x, n_times_per_iteration))
-        grid_running_avg_w = Array{Float64, 3}(undef, (num_samples_y, num_samples_x, n_times_per_iteration))
-        grid_sample_cnts = Array{UInt16, 2}(undef, (num_samples_y, num_samples_x))
-
-        println("Done.")
+        grid_sample_counts = Array{UInt16, 2}(undef, (num_samples_y, num_samples_x))
 
         #============================================#
         # Sample tetrahedra and write to output
@@ -137,42 +137,49 @@ module Rasterization
         println("RAM limit: ", Util.human_readable_size(mem_limit), "; Can work on ", n_times_per_iteration, 
                 " timesteps at once (therefore needing ", n_iterations , " iterations).")
 
-        start_time :: DateTime = now()
-        last_printed_time = start_time
-        print_interval = Second(2)
-
         for iteration ∈ 1:n_iterations
             n_times = min(length(times) - (iteration - 1) * n_times_per_iteration, n_times_per_iteration)
-            @inbounds grid_running_avg_u[:,:,1:n_times] .= 0
-            @inbounds grid_running_avg_v[:,:,1:n_times] .= 0
-            @inbounds grid_running_avg_w[:,:,1:n_times] .= 0
-            grid_sample_cnts .= 0
+
+            prefetched_vars = Array{Float64, 3}(undef, (n_tetrahedra, n_times, n_vars))
+            @profile begin
+                for var_id ∈ 1:n_vars, time ∈ 1:n_times
+                    dest_offset = (var_id-1)*n_times*n_tetrahedra + (time-1)*n_tetrahedra + 1
+                    copyto!(prefetched_vars, dest_offset, vars[time, var_id], 1, n_tetrahedra)
+                end
+            end # profile
+
+            for grid in iteration_grids
+                grid[1:n_times,:,:] .= 0
+            end
 
             Threads.@threads for thread_id ∈ 1:n_threads
-                iterate_tets!(thread_id, n_threads, n_tetrahedra, 
-                    tetrahedra, points, bin_ids, vars,
-                    domain_x, domain_y, domain_z, sampling_rate,
-                    n_times, iteration, total_problems, n_times_per_iteration,
-                    grid_sample_cnts, grid_running_avg_u, grid_running_avg_v, grid_running_avg_w)
+                    iterate_tets!(thread_id, n_threads, n_tetrahedra, 
+                        tetrahedra, points, bin_ids, prefetched_vars,
+                        domain_x, domain_y, domain_z, sampling_rate,
+                        num_samples_x, num_samples_y, num_samples_z,
+                        n_times, iteration, total_problems, n_times_per_iteration,
+                        bin_counts, iteration_grids, grid_sample_counts)
             end # for thread_id
+
+            println()
+            print("Processing tets on bucket borders... ")
 
             # Now, iterate over the remaining tets that lie ON bin edges and therefore could not be processed in parallel
             iterate_tets!(0, n_threads, n_tetrahedra, 
-                tetrahedra, points, bin_ids, vars,
+                tetrahedra, points, bin_ids, prefetched_vars,
                 domain_x, domain_y, domain_z, sampling_rate,
+                num_samples_x, num_samples_y, num_samples_z,
                 n_times, iteration, total_problems, n_times_per_iteration,
-                grid_sample_cnts, grid_running_avg_u, grid_running_avg_v, grid_running_avg_w)
+                bin_counts, iteration_grids, grid_sample_counts)
 
-            open("profile.txt", "w") do p
-                Profile.print(p)
-            end
+            println("Done.")
 
             start = [1, 1, (iteration - 1) * n_times_per_iteration + 1]
             count = [num_samples_y, num_samples_x, n_times]
 
-            ncwrite(grid_running_avg_u, out_filename, "u", start=start, count=count)
-            ncwrite(grid_running_avg_v, out_filename, "v", start=start, count=count)
-            ncwrite(grid_running_avg_w, out_filename, "w", start=start, count=count)
+            for var_id ∈ 1:n_vars
+                ncwrite(iteration_grids[var_id], out_filename, var_names[var_id], start=start, count=count)
+            end
         end # for iteration
 
         println()
@@ -180,22 +187,30 @@ module Rasterization
 
     @inline function iterate_tets!(thread_id, n_threads, n_tetrahedra, tetrahedra, points, bin_ids, vars, 
                           domain_x, domain_y, domain_z, sampling_rate, 
+                          num_samples_x, num_samples_y, num_samples_z,
                           n_times, iteration, total_problems, n_times_per_iteration,
-                          grid_sample_cnts, grid_running_avg_u, grid_running_avg_v, grid_running_avg_w)
+                          bin_counts, iteration_grids, grid_sample_counts)
 
-        ctxt = ThreadContext(
-            Array{Float64, 2}(undef, (4, 3)),
-            Array{Float64, 2}(undef, (3, 2)),
-            Array{UInt8, 2}(undef, (64, 64)),
-            Array{Float64, 1}(undef, 3),
-            Array{SubArray, 1}(undef, 4),
-            Array{Float64, 1}(undef, 3),
-            Array{TetrahedronFace, 1}(undef, 4))
+        tet_points  = Array{Float64, 2}(undef, (4, 3))
+        tet_aabb    = Array{Float64, 2}(undef, (3, 2))
+        tet_dict    = Array{UInt8, 2}(undef, (64, 64))
+    
+        face_points = Array{SubArray, 1}(undef, 4)
+        tet_faces   = Array{TetrahedronFace, 1}(undef, 4)
+
+        p           = Array{Float64, 1}(undef, 3)
+        n           = Array{Float64, 1}(undef, 3)
+
+        bin_width = (thread_id < n_threads) ? num_samples_x ÷ n_threads : num_samples_x - (n_threads-1)*(num_samples_x÷n_threads)
+        bin_offset = (thread_id-1)*(num_samples_x÷n_threads)
+
+        println("Thread $thread_id has bin width $bin_width")
 
         start_time = now()
         last_printed_time = start_time
         print_interval = Second(2)
-        solved_problems = 0
+        rasterized_tets = 0
+        total_tets = bin_counts[thread_id+1]
 
         for tet_id ∈ 1:n_tetrahedra
             # Only process tetrahedra in thread's own bin
@@ -205,63 +220,53 @@ module Rasterization
 
             tetrahedron = (@view tetrahedra[:, tet_id])
             for i ∈ 1:4, dim ∈ 1:3
-                ctxt.tet_points[i, dim] = points[dim, tetrahedron[i]]
+                tet_points[i, dim] = points[dim, tetrahedron[i]]
             end
 
             for dim ∈ 1:3
-                ctxt.tet_aabb[dim, 1] = minimum(ctxt.tet_points[:, dim])
-                ctxt.tet_aabb[dim, 2] = maximum(ctxt.tet_points[:, dim])
+                tet_aabb[dim, 1] = minimum(tet_points[:, dim])
+                tet_aabb[dim, 2] = maximum(tet_points[:, dim])
             end
 
-            @inbounds x_min = floor(Int32,(ctxt.tet_aabb[1,1] - domain_x[1]) / sampling_rate[1]) + 1
-            @inbounds y_min = floor(Int32,(ctxt.tet_aabb[2,1] - domain_y[1]) / sampling_rate[2]) + 1
-            @inbounds z_min = floor(Int32,(ctxt.tet_aabb[3,1] - domain_z[1]) / sampling_rate[3]) + 1
+            @inbounds x_min = floor(Int32,(tet_aabb[1,1] - domain_x[1]) / sampling_rate[1]) + 1
+            @inbounds y_min = floor(Int32,(tet_aabb[2,1] - domain_y[1]) / sampling_rate[2]) + 1
+            @inbounds z_min = floor(Int32,(tet_aabb[3,1] - domain_z[1]) / sampling_rate[3]) + 1
     
-            @inbounds x_max = ceil(Int32,(ctxt.tet_aabb[1,2] - domain_x[1]) / sampling_rate[1])
-            @inbounds y_max = ceil(Int32,(ctxt.tet_aabb[2,2] - domain_y[1]) / sampling_rate[2])
-            @inbounds z_max = ceil(Int32,(ctxt.tet_aabb[3,2] - domain_z[1]) / sampling_rate[3])
+            @inbounds x_max = ceil(Int32,(tet_aabb[1,2] - domain_x[1]) / sampling_rate[1])
+            @inbounds y_max = ceil(Int32,(tet_aabb[2,2] - domain_y[1]) / sampling_rate[2])
+            @inbounds z_max = ceil(Int32,(tet_aabb[3,2] - domain_z[1]) / sampling_rate[3])
 
             if thread_id == 1
                 t_now = now()
-                if (t_now - last_printed_time) >= print_interval && solved_problems > 0
+                if (t_now - last_printed_time) >= print_interval && rasterized_tets > 0
 
                     last_printed_time = t_now
-                    etr = (t_now-start_time) * (total_problems ÷ n_threads - solved_problems) ÷ (solved_problems)
+                    etr = (t_now-start_time) * (total_tets - rasterized_tets) ÷ (rasterized_tets)
                     hh = floor(etr, Dates.Hour)
                     etr -= floor(hh, Dates.Millisecond)
                     mm = floor(etr, Dates.Minute)
                     etr -= floor(mm, Dates.Millisecond)
                     ss = floor(etr, Dates.Second)
 
-                    @printf("Working on tetrahedron %d in timesteps %d-%d (%2.2f%% done). ETR: %02d:%02d:%02d    \r", 
-                            solved_problems, 
+                    @printf("Working on tetrahedron %d of %d in timesteps %d-%d (%2.2f%% done). ETR: %02d:%02d:%02d    \r", 
+                            rasterized_tets, total_tets,
                             (iteration - 1) * n_times_per_iteration + 1,
                             (iteration - 1) * n_times_per_iteration + n_times,
-                            solved_problems/(total_problems/n_threads)*100,
+                            rasterized_tets/total_tets*100,
                             hh.value, mm.value, ss.value)
                 end
             end # if thread_id
 
             num_current_cells_x = x_max - x_min + 1
             num_current_cells_y = y_max - y_min + 1
-            if size(ctxt.tet_dict, 1) < num_current_cells_y || size(ctxt.tet_dict, 2) < num_current_cells_x
-                ctxt.tet_dict = Array{UInt8, 2}(undef, (num_current_cells_y * 2, num_current_cells_x * 2))
+            if size(tet_dict, 1) < num_current_cells_y || size(tet_dict, 2) < num_current_cells_x
+                tet_dict = Array{UInt8, 2}(undef, (num_current_cells_y * 2, num_current_cells_x * 2))
             end
 
-            @inbounds ctxt.tet_dict[1:y_max-y_min+1, 1:x_max-x_min+1] .= 0
+            @inbounds tet_dict[1:y_max-y_min+1, 1:x_max-x_min+1] .= 0
 
             for i ∈ 1:4
-                @inbounds ctxt.face_points[i] = @view ctxt.tet_points[i,:]
-            end
-
-            function cross3!(a, b, ret)
-                @inbounds ret[1] = (a[2]*b[3] - a[3]*b[2])
-                @inbounds ret[2] = (a[3]*b[1] - a[1]*b[3])
-                @inbounds ret[3] = (a[1]*b[2] - a[2]*b[1])
-            end
-
-            function dot3(a, b)
-                @inbounds return a[1]*b[1] + a[2]*b[2] + a[3]*b[3]
+                @inbounds face_points[i] = @view tet_points[i,:]
             end
 
             # Array{(n, d, dist_p_excl)}
@@ -269,44 +274,44 @@ module Rasterization
                 # Exclude point i from the tetrahedron and consider the plane defined by the other 3 points
                 # If and only if (x,y,z) lies on the "positive" side (in normal direction) of all 4 planes, 
                 # it is inside the tetrahedron
-                @inbounds p_excl =    ctxt.face_points[i]
-                @inbounds p1 =        ctxt.face_points[(i%4)+1]
-                @inbounds p2 =        ctxt.face_points[((i+1)%4)+1]
-                @inbounds p3 =        ctxt.face_points[((i+2)%4)+1]
+                @inbounds p_excl =    face_points[i]
+                @inbounds p1 =        face_points[(i%4)+1]
+                @inbounds p2 =        face_points[((i+1)%4)+1]
+                @inbounds p3 =        face_points[((i+2)%4)+1]
             
                 # Calculate Hesse normal form of the plane defined by p1, p2, p3
                 # Normal vector
-                cross3!(p2-p1, p3-p1, ctxt.n)
-                normalize!(ctxt.n)
+                cross3!(p2-p1, p3-p1, n)
+                normalize!(n)
             
                 # n1x1 + n2x2 + n3x3 + d = 0; solve for d
-                d = -dot3(p1, ctxt.n)
-                dist_p_excl = dot3(p_excl, ctxt.n) + d
+                d = -dot3(p1, n)
+                dist_p_excl = dot3(p_excl, n) + d
 
                 # Ensure that p_excl is considered to be on the "positive" side of the plane
                 if dist_p_excl < 0
-                    ctxt.n = -ctxt.n
+                    n = -n
                     d = -d
                     dist_p_excl = -dist_p_excl
                 end
 
-                @inbounds ctxt.tet_faces[i] = TetrahedronFace((ctxt.n[1], ctxt.n[2], ctxt.n[3]), d, dist_p_excl)
+                @inbounds tet_faces[i] = TetrahedronFace((n[1], n[2], n[3]), d, dist_p_excl)
             end # for i
 
             for x ∈ x_min:x_max
-                @inbounds ctxt.p[1] = (x-1)*sampling_rate[1] + domain_x[1]
+                @inbounds p[1] = (x-1)*sampling_rate[1] + domain_x[1]
                 for y ∈ y_min:y_max
-                    @inbounds ctxt.p[2] = (y-1)*sampling_rate[2] + domain_y[1]
+                    @inbounds p[2] = (y-1)*sampling_rate[2] + domain_y[1]
                     for z ∈ z_min:z_max
-                        @inbounds ctxt.p[3] = (z-1)*sampling_rate[3] + domain_z[1]
+                        @inbounds p[3] = (z-1)*sampling_rate[3] + domain_z[1]
                     
                         accepted = true
                         # For each tetrahedron face, filter out cells that are not in the tetrahedron
                         for i ∈ 1:4
-                            @inbounds face = ctxt.tet_faces[i]
+                            @inbounds face = tet_faces[i]
                             # p_excl is ALWAYS on the "positive" side of the plane. 
                             # So, if p_excl and p lie on the same side, p is inside the tetrahedron
-                            dist_p = dot3(ctxt.p, face.n) + face.d
+                            dist_p = dot3(p, face.n) + face.d
                         
                             # The point lies approx. ON the face, reject in the tet on the lexicographically
                             # more negative side of the face. This ensures that for two neighboring tetrahedra, the point is
@@ -317,9 +322,9 @@ module Rasterization
                             # normal vector in contrast to its negated counterpart.
                             if (abs(dist_p) <= EDGE_TOL) 
                                 @inbounds is_positive_side = 
-                                    (ctxt.n[1] > 0.) || 
-                                    (ctxt.n[1] == 0. && ctxt.n[2] > 0.) || 
-                                    (ctxt.n[1] == 0. && ctxt.n[2] == 0. && ctxt.n[3] > 0.)
+                                    (n[1] > 0.) || 
+                                    (n[1] == 0. && n[2] > 0.) || 
+                                    (n[1] == 0. && n[2] == 0. && n[3] > 0.)
 
                                 # Reject if in tetrahedron on face's lex. negative side
                                 if !is_positive_side
@@ -339,26 +344,37 @@ module Rasterization
 
                         if accepted # accept cell and increment its 2D cell counter for the current tetrahedron by 1
                             x_ = x - x_min + 1; y_ = y - y_min + 1
-                            @inbounds ctxt.tet_dict[y_,x_] += 1
+                            @inbounds tet_dict[y_,x_] += 1
                         end
                     end # for z
                 end # for y
             end # for x
 
             for x ∈ 1:x_max-x_min+1, y ∈ 1:y_max-y_min+1
-                @inbounds num_samples = ctxt.tet_dict[y, x]
+                @inbounds num_samples = tet_dict[y, x]
                 if num_samples > 0
                     x_ = x + x_min - 1; y_ = y + y_min - 1
-                    @inbounds total_samples = (grid_sample_cnts[y_, x_] += num_samples)
-                    @simd for t ∈ 1:n_times
-                        @inbounds grid_running_avg_u[y_, x_, t] += (vars[t,1][tet_id]-grid_running_avg_u[y_, x_, t])*(num_samples/total_samples)
-                        @inbounds grid_running_avg_v[y_, x_, t] += (vars[t,2][tet_id]-grid_running_avg_v[y_, x_, t])*(num_samples/total_samples)
-                        @inbounds grid_running_avg_w[y_, x_, t] += (vars[t,3][tet_id]-grid_running_avg_w[y_, x_, t])*(num_samples/total_samples)
+                    @inbounds total_samples = (grid_sample_counts[y_, x_] += num_samples)
+                    for var_id ∈ 1:3
+                        for t ∈ 1:n_times
+                            @inbounds avg = iteration_grids[var_id][y_, x_, t]
+                            @inbounds iteration_grids[var_id][y_, x_, t] = avg + (vars[tet_id, t, var_id]-avg)*(num_samples/total_samples)
+                        end
                     end
                 end
             end
 
-            solved_problems += n_times
+            rasterized_tets += 1
         end # for tet_id
+    end
+
+    @inline function cross3!(a, b, ret)
+        @inbounds ret[1] = (a[2]*b[3] - a[3]*b[2])
+        @inbounds ret[2] = (a[3]*b[1] - a[1]*b[3])
+        @inbounds ret[3] = (a[1]*b[2] - a[2]*b[1])
+    end
+
+    @inline function dot3(a, b)
+        @inbounds return a[1]*b[1] + a[2]*b[2] + a[3]*b[3]
     end
 end # module Rasterization
