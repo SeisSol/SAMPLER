898beee709adf0b56c664129cee6b0b0bf3122e7
Maximilian Schmeller <m.schmeller@tum.de>
2021-07-01 13:23:35 +0200

    Activated Git LFS, added example inputs for unit tests

---
+++ test/io.jl
@@ -0,0 +1,18 @@
+include("../io/args.jl")
+include("../io/xdmf.jl")
+include("../io/netcdf.jl")
+
+using Test
+using Main.Args
+using Main.XDMF
+using Main.NetCDF
+
+@testset "IO" begin
+    @testset "NetCDF" begin
+        
+    end
+
+    @testset "XDMF" begin
+        
+    end
+end
